4f80d9df490252f819594275c5e9c9daa5b35496
casibbald <casibbald@gmail.com>
2025-12-26 19:49:51 +0200

    fix: Add test modules to ipam/mod.rs

--- controllers/netbox/src/reconciler/ipam/mod.rs
+++ controllers/netbox/src/reconciler/ipam/mod.rs
@@ -3,8 +3,12 @@
 //! Handles: IPClaim, IPPool, NetBoxPrefix, NetBoxAggregate
 
 pub mod ip_claim;
+#[cfg(test)]
+pub mod ip_claim_test;
 pub mod ip_pool;
 #[cfg(test)]
-mod ip_pool_test;
+pub mod ip_pool_test;
 pub mod prefix;
+#[cfg(test)]
+pub mod prefix_test;
 pub mod aggregate;
